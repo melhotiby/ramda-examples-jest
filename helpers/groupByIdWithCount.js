4115ae6b413db68de848da9ead70e49a5ed96c0a
Matt Elhotiby <hotiby@gmail.com>
2021-03-17 15:34:15 -0400

    More ramda cleanup

--- helpers/groupByIdWithCount.js
+++ helpers/groupByIdWithCount.js
@@ -27,7 +27,7 @@ import {
 const groupByname = prop('name')
 const byGrade = groupBy((data) => groupByname(data))
 
-const buildObject = (data, key) => {
+const buildObject = curry((data, key) => {
   const elementData = data[key]
   const filteredData = map(prop(['cost']))(elementData)
   const getId = compose(head, map(prop(['id'])))(elementData)
@@ -38,13 +38,9 @@ const buildObject = (data, key) => {
     total: sum(filteredData),
     count: length(filteredData),
   }
-}
+})
 
-const getSums = (data) =>
-  compose(
-    map((key) => buildObject(data, key)),
-    keys
-  )(data)
+const getSums = (data) => compose(map(buildObject(data)), keys)(data)
 
 const groupByIdWithCount = compose(
   sortBy(prop('id')),
